c6dac7bd0a9ea9348bc97bf63b9bd47d465fb63c
nileshseth007 <nileshseth_co20b5_35@dtu.ac.in>
2023-01-30 21:53:42 +0530

    final commit

--- app.js
+++ app.js
@@ -5,49 +5,54 @@ const bodyParser = require('body-parser');
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 
-//encryption package in mongoose Uses AES 
-// for installing npm i mongoose-encryption 
-// can encrypt and authenticate
-const encrypt = require("mongoose-encryption");
+// for managing sessions
+const session = require("express-session");
+
+// authentication package for NodeJs
+const passport = require("passport");
+//Mongoose plugin that simplifies building username and password login with Passport.
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app= express();
 
-mongoose.set("strictQuery", false);
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+// under all the app.use/set and above the mongoose.connect
+// helps creating new sessions 
+app.use(session({
+    secret: 'Our little secret.',
+    resave: false,
+    saveUninitialized: false
+}));
 
+// tell our app to start using authentication and sessions
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.set("strictQuery", false);
 mongoose.connect('mongodb://127.0.0.1:27017/secretsDB', function(err){
     if(err) console.log(err);
     else console.log("Connected successfully to mongoDB");
 });
 
-// object of the mongoose schema class
+// schema must be mongoose schema in order to use passport
 const userSchema = new mongoose.Schema({
-    email: String,
+    username: String,
     password: String 
 })
 
-// key for encryption (convienient in docs)
-// if hacker found the file app.js then they can easily get the secret key
-// this makes database vulnerable to attack hence use an env variable
-// const secret = "Thisisourdirtylittlesecret";
+// hash and salt the password and  then store in DB
+userSchema.plugin(passportLocalMongoose);
 
+const User = mongoose.model("User", userSchema);
 
-// add plugin to our schema which encrypts the defined fields using secret as key
-// plugins are extra functionality added to schema to enhance its function and power
+// write this after the model, it provides authentication to model
+passport.use(User.createStrategy());
 
-// This must be done before defining any model eg - User
-// email is not encrypted to allow searching of the details
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
-
-const User = new mongoose.model("User", userSchema);
-
-// to make static css into dynamcic
-app.use(express.static("public"));
-
-// telling the server to see for the ejs file is a views folder
-app.set("view engine", "ejs");
-
-// allows to get into several elements of body of a request
-app.use(bodyParser.urlencoded({extended:true}));
+// use to create cookie for the session and stuff user's info as msg in cookie
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get('/',function(req,res){
     res.render("home");
@@ -61,33 +66,62 @@ app.get('/register',function(req,res){
     res.render("register");
 });
 
-// to recieve the data from form and use it directy
-app.post("/register", function(req,res){
-    const newUser = new User({
-        email : req.body.email,
-        password : req.body.password
-    })
-
-    // encrypts when a save function is executed
-    // decrypts when find function is called
-    newUser.save(function(err){
-        if(err) console.log(err);
-        else{
-            res.render("secrets");
+app.get("/logout", function(req,res){
+    // callback for log out
+    req.logout(function(err) {
+        if (err) { 
+            return next(err); 
         }
+        // successfully logs out and redirect to root route
+        res.redirect('/');
+      });
+})
+
+app.get("/secrets", function(req,res){
+
+    // if this page is directly accessed from root route then it autheticates and then 
+    // redirects it to the secrets page
+    if(req.isAuthenticated()){
+        res.render("secrets");
+    }
+    else res.redirect("/login");
+})
+
+app.post("/register", function(req,res){
+    
+    User.register({username: req.body.username}, req.body.password, function(err, user){
+        if(err) {
+            console.log(err);
+            res.redirect("/register");
+        }
+        else{
+            // authenticates the user details and redirects to secrets page
+            passport.authenticate("local")(req,res,function(){
+                res.redirect("/secrets");
+            })
+        }
+
     })
 })
 
 app.post("/login", function(req,res){
-    const username = req.body.email;
-    const password = req.body.password;
 
-    // mongoose-encrypt decrypts when find function is used
-    User.findOne({email: username}, function(err, foundUser){
-        if(err) console.log(err);
-        else{
-            if(foundUser.password === password) res.render("secrets");
+    const user = new User({
+        username : req.body.username,
+        password: req.body.password
+    })
+    req.login(user, function(err, user){
+        if(err) {
+            console.log(err);
+            res.redirect("/login");
         }
+        else{
+            // authenticates the entered details and matched with what is stored in DB
+            passport.authenticate("local")(req,res,function(){
+                res.redirect("/secrets");
+            })
+        }
+
     })
 })
 
